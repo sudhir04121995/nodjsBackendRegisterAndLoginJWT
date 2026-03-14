c642632115669ba638182fd069a9dd6a03a3f667
Sudharsanan <mechsudharsanan555@gmail.com>
2026-03-14 22:15:32 +0530

    registration and login are saved in auth folder

---
+++ auth/registration.js
@@ -0,0 +1,25 @@
+const user = require("../model/user")
+const bcrypt = require("bcryptjs");
+
+exports.register =  async(req,res)=>{
+    try {
+        const {name,email,password}= req.body;
+        const userExist = await user.findOne({email})
+        if(userExist){
+            return res.status(400).json({message:"user already exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword= await bcrypt.hash(password,salt);
+
+        const newUser = new user({
+            name,
+            email,
+            password:hashedPassword
+    })
+    await newUser.save()
+   res.json({message:"User Registered Successfully"})
+} catch (error) {
+         res.status(500).json({ error: error.message });
+    }
+}
