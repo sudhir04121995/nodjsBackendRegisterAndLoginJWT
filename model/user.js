c9e8746d572d6fc77584c07d72547cde02c13360
Sudharsanan <mechsudharsanan555@gmail.com>
2026-03-15 13:41:43 +0530

    validation added to thr password in user model

--- model/user.js
+++ model/user.js
@@ -10,10 +10,17 @@ const userSchema = new mongoose.Schema({
         require:true,
         unique:true
     },
-    password:{
-        type:String,
-        required:true
-    }
+   password: {
+  type: String,
+  required: true,
+  validate: {
+    validator: function (password) {
+      return /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/.test(password);
+    },
+    message:
+      "Password must be at least 8 characters and include 1 number and 1 special character"
+  }
+}
 });
 
 module.exports =mongoose.model("user", userSchema);
